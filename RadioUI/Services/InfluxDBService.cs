8804faad22f55df3367244dce7eff97782996ef7
adk86 <adk8673@gmail.com>
2024-12-01 23:49:24 -0600

    Add new UI project

---
+++ RadioUI/Services/InfluxDBService.cs
@@ -0,0 +1,23 @@
+﻿using InfluxDB.Client;
+
+namespace RadioUI.Services
+{
+    public class InfluxDBService
+    {
+        private readonly string _token;
+
+        public InfluxDBService(IConfiguration configuration)
+        {
+            _token = configuration.GetValue<string>("InfluxDB:Token") ?? string.Empty;
+        }
+
+        public async Task<T> QueryAsync<T>(Func<QueryApi, Task<T>> action)
+        {
+            using (var client = new InfluxDBClient("http://localhost:8086"))
+            {
+                var query = client.GetQueryApi();
+                return await action(query);
+            }
+        }
+    }
+}
